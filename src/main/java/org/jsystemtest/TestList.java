ef2ccb315eb2104b27e29d1e3ea81e711ec5a7ae
Ben-mark <bm7382598@gmail.com>
2017-08-02 18:57:10 +0300

    jsystem bb debug plugin update
    supports RunnerTest pause icon rendering, handles all possible status combinations rendering, ScenarioAsTest however, is still in development, soon to be published

--- src/main/java/org/jsystemtest/TestList.java
+++ src/main/java/org/jsystemtest/TestList.java
@@ -6,12 +6,13 @@ import java.util.Iterator;
 import jsystem.framework.scenario.JTest;
 
 public class TestList extends ArrayList<JTest> {
-
-	/**
+	
+	 /**
 	 * 
-	 */
+	 **/
+	
 	private static final long serialVersionUID = 1L;
-
+	
 	
 	@Override
 	public boolean contains(Object o){
@@ -22,8 +23,7 @@ public class TestList extends ArrayList<JTest> {
 		for (int i = 0 ; i < this.size() ; i++){
 			if (test.getFullUUID().equals(this.get(i).getFullUUID())){
 				return true;
-			}
-			
+			}	
 		}
 		return false;
 	}
