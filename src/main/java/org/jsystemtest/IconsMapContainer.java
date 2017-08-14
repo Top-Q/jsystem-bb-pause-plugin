37299f64d3295307ca3c8bebeb37370668a45dc0
Ben-Mark <bm7382598@gmail.com>
2017-08-14 18:24:04 +0300

    scenarioAsTest feature has been added to the pause plugin (#2)
    added all scenarioAsTest paused icon combinations, fetched from IconsMapContainer, logic has been implemented in ScenarioRendererWithPause

--- src/main/java/org/jsystemtest/IconsMapContainer.java
+++ src/main/java/org/jsystemtest/IconsMapContainer.java
@@ -15,90 +15,98 @@ public class IconsMapContainer {
 	private HashMap<String,ImageIcon> pausedMarkedIsseuMap;
 	private HashMap<String,ImageIcon> pausedMarkedNegIssueMap;
 	
+	private HashMap<String,ImageIcon> scenarioAsTestPausedNormalMap;
+	private HashMap<String,ImageIcon> scenarioAsTestpausedMarkedNegMap;
+	private HashMap<String,ImageIcon> scenarioAsTestpausedMarkedIsseuMap;
+	private HashMap<String,ImageIcon> scenarioAsTestpausedMarkedNegIssueMap;
+	
 	public IconsMapContainer(){
 		
 		try {
 			ClassLoader cl = this.getClass().getClassLoader();	
 			
 			pausedNormalMap = new HashMap<>();
-			pausedNormalMap.put("pausedPlainRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/plainTest/pausedRunPlain.gif"))));
-			pausedNormalMap.put("pausedPlain", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/plainTest/pausedPlain.gif"))));
-			pausedNormalMap.put("pausedOk", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testSuccess/pausedOk.gif"))));
-			pausedNormalMap.put("pausedErr", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testError/pausedError.gif"))));
-			pausedNormalMap.put("pausedWarn", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testWarning/pausedWarning.gif"))));
-			pausedNormalMap.put("pausedFail", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testFailure/pausedFail.gif"))));
-			pausedNormalMap.put("pausedFailRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testFailure/pausedFailRun.gif"))));
-			pausedNormalMap.put("pausedOkRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testSuccess/pausedOkRun.gif"))));
-			pausedNormalMap.put("pausedErrRun",  new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testError/pausedErrorRun.gif"))));
-			pausedNormalMap.put("pausedWarnRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testWarning/pausedWarningRun.gif"))));
+			pausedNormalMap.put("pausedPlainRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/plainTest/pausedRunPlain.gif"))));
+			pausedNormalMap.put("pausedPlain", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/plainTest/pausedPlain.gif"))));
+			pausedNormalMap.put("pausedOk", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testSuccess/pausedOk.gif"))));
+			pausedNormalMap.put("pausedErr", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testError/pausedError.gif"))));
+			pausedNormalMap.put("pausedWarn", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testWarning/pausedWarning.gif"))));
+			pausedNormalMap.put("pausedFail", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testFailure/pausedFail.gif"))));
+			pausedNormalMap.put("pausedFailRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testFailure/pausedFailRun.gif"))));
+			pausedNormalMap.put("pausedOkRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testSuccess/pausedOkRun.gif"))));
+			pausedNormalMap.put("pausedErrRun",  new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testError/pausedErrorRun.gif"))));
+			pausedNormalMap.put("pausedWarnRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testWarning/pausedWarningRun.gif"))));
 			
 			pausedMarkedNegMap = new HashMap<>();
-			pausedMarkedNegMap.put("pausedPlainRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/plainTest/pausedRunNeg.gif"))));
-			pausedMarkedNegMap.put("pausedPlain", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/plainTest/pausedTestNeg.gif"))));
-			pausedMarkedNegMap.put("pausedOk", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testSuccess/pausedOkNeg.gif"))));
-			pausedMarkedNegMap.put("pausedErr", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testError/pausedErrorNeg.gif"))));
-			pausedMarkedNegMap.put("pausedWarn", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testWarning/pausedWarningNeg.gif"))));
-			pausedMarkedNegMap.put("pausedFail", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testFailure/pausedFailNeg.gif"))));
-			pausedMarkedNegMap.put("pausedFailRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testFailure/pausedFailRunNeg.gif"))));
-			pausedMarkedNegMap.put("pausedOkRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testSuccess/pausedOkRunNeg.gif"))));
-			pausedMarkedNegMap.put("pausedErrRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testError/pausedErrorRunNeg.gif"))));
-			pausedMarkedNegMap.put("pausedWarnRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testWarning/pausedWarningRunNeg.gif"))));
+			pausedMarkedNegMap.put("pausedPlainRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/plainTest/pausedRunNeg.gif"))));
+			pausedMarkedNegMap.put("pausedPlain", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/plainTest/pausedTestNeg.gif"))));
+			pausedMarkedNegMap.put("pausedOk", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testSuccess/pausedOkNeg.gif"))));
+			pausedMarkedNegMap.put("pausedErr", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testError/pausedErrorNeg.gif"))));
+			pausedMarkedNegMap.put("pausedWarn", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testWarning/pausedWarningNeg.gif"))));
+			pausedMarkedNegMap.put("pausedFail", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testFailure/pausedFailNeg.gif"))));
+			pausedMarkedNegMap.put("pausedFailRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testFailure/pausedFailRunNeg.gif"))));
+			pausedMarkedNegMap.put("pausedOkRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testSuccess/pausedOkRunNeg.gif"))));
+			pausedMarkedNegMap.put("pausedErrRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testError/pausedErrorRunNeg.gif"))));
+			pausedMarkedNegMap.put("pausedWarnRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testWarning/pausedWarningRunNeg.gif"))));
 			
 			pausedMarkedIsseuMap = new HashMap<>();
-			pausedMarkedIsseuMap.put("pausedPlainRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/plainTest/pausedRunIssue.gif"))));
-			pausedMarkedIsseuMap.put("pausedPlain", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/plainTest/pausedIssue.gif"))));
-			pausedMarkedIsseuMap.put("pausedOk", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testSuccess/pausedOkIssue.gif"))));
-			pausedMarkedIsseuMap.put("pausedErr", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testError/pausedErrorIssue.gif"))));
-			pausedMarkedIsseuMap.put("pausedWarn", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testWarning/pausedWarningIssue.gif"))));
-			pausedMarkedIsseuMap.put("pausedFail", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testFailure/pausedFailIssue.gif"))));
-			pausedMarkedIsseuMap.put("pausedFailRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testFailure/pausedFailRunIssue.gif"))));
-			pausedMarkedIsseuMap.put("pausedOkRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testSuccess/pausedOkRunIssue.gif"))));
-			pausedMarkedIsseuMap.put("pausedErrRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testError/pausedErrorRunIssue.gif"))));
-			pausedMarkedIsseuMap.put("pausedWarnRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testWarning/pausedWarningRunIssue.gif"))));
+			pausedMarkedIsseuMap.put("pausedPlainRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/plainTest/pausedRunIssue.gif"))));
+			pausedMarkedIsseuMap.put("pausedPlain", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/plainTest/pausedIssue.gif"))));
+			pausedMarkedIsseuMap.put("pausedOk", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testSuccess/pausedOkIssue.gif"))));
+			pausedMarkedIsseuMap.put("pausedErr", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testError/pausedErrorIssue.gif"))));
+			pausedMarkedIsseuMap.put("pausedWarn", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testWarning/pausedWarningIssue.gif"))));
+			pausedMarkedIsseuMap.put("pausedFail", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testFailure/pausedFailIssue.gif"))));
+			pausedMarkedIsseuMap.put("pausedFailRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testFailure/pausedFailRunIssue.gif"))));
+			pausedMarkedIsseuMap.put("pausedOkRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testSuccess/pausedOkRunIssue.gif"))));
+			pausedMarkedIsseuMap.put("pausedErrRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testError/pausedErrorRunIssue.gif"))));
+			pausedMarkedIsseuMap.put("pausedWarnRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testWarning/pausedWarningRunIssue.gif"))));
 			
 			pausedMarkedNegIssueMap = new HashMap<>();
-			pausedMarkedNegIssueMap.put("pausedPlainRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/plainTest/pausedRunNegIssue.gif"))));
-			pausedMarkedNegIssueMap.put("pausedPlain", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/plainTest/pausedIssueNeg.gif"))));
-			pausedMarkedNegIssueMap.put("pausedOk", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testSuccess/pausedOkIssueNeg.gif"))));
-			pausedMarkedNegIssueMap.put("pausedErr", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testError/pausedErrorIssueNeg.gif"))));
-			pausedMarkedNegIssueMap.put("pausedWarn", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testWarning/pausedWarningNegIssue.gif"))));
-			pausedMarkedNegIssueMap.put("pausedFail", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testFailure/pausedFailNegIssue.gif"))));
-			pausedMarkedNegIssueMap.put("pausedFailRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testFailure/pausedFailRunNegIssue.gif"))));
-			pausedMarkedNegIssueMap.put("pausedOkRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testSuccess/pausedOkRunIssueNeg.gif"))));
-			pausedMarkedNegIssueMap.put("pausedErrRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testError/pausedErrorRunIssueNeg.gif"))));
-			pausedMarkedNegIssueMap.put("pausedWarnRun", new ImageIcon(
-					IOUtils.toByteArray(cl.getResourceAsStream("iconsByStatus/testWarning/pausedWarningRunNegIssue.gif"))));
+			pausedMarkedNegIssueMap.put("pausedPlainRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/plainTest/pausedRunNegIssue.gif"))));
+			pausedMarkedNegIssueMap.put("pausedPlain", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/plainTest/pausedIssueNeg.gif"))));
+			pausedMarkedNegIssueMap.put("pausedOk", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testSuccess/pausedOkIssueNeg.gif"))));
+			pausedMarkedNegIssueMap.put("pausedErr", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testError/pausedErrorIssueNeg.gif"))));
+			pausedMarkedNegIssueMap.put("pausedWarn", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testWarning/pausedWarningNegIssue.gif"))));
+			pausedMarkedNegIssueMap.put("pausedFail", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testFailure/pausedFailNegIssue.gif"))));
+			pausedMarkedNegIssueMap.put("pausedFailRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testFailure/pausedFailRunNegIssue.gif"))));
+			pausedMarkedNegIssueMap.put("pausedOkRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testSuccess/pausedOkRunIssueNeg.gif"))));
+			pausedMarkedNegIssueMap.put("pausedErrRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testError/pausedErrorRunIssueNeg.gif"))));
+			pausedMarkedNegIssueMap.put("pausedWarnRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("testIconsByStatus/testWarning/pausedWarningRunNegIssue.gif"))));
+			
+			scenarioAsTestPausedNormalMap = new HashMap<>();
+			scenarioAsTestPausedNormalMap.put("pausedPlainRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/plainTest/pausedRunPlain.gif"))));
+			scenarioAsTestPausedNormalMap.put("pausedPlain", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/plainTest/pausedPlain.gif"))));
+			scenarioAsTestPausedNormalMap.put("pausedOk", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testSuccess/pausedOk.gif"))));
+			scenarioAsTestPausedNormalMap.put("pausedErr", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testError/pausedError.gif"))));
+			scenarioAsTestPausedNormalMap.put("pausedWarn", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testWarning/pausedWarning.gif"))));
+			scenarioAsTestPausedNormalMap.put("pausedFail", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testFailure/pausedFail.gif"))));
+			
+			scenarioAsTestpausedMarkedNegMap = new HashMap<>();
+			scenarioAsTestpausedMarkedNegMap.put("pausedPlainRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/plainTest/pausedPlainNegRun.gif"))));
+			scenarioAsTestpausedMarkedNegMap.put("pausedPlain", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/plainTest/pausedPlainNeg.gif"))));
+			scenarioAsTestpausedMarkedNegMap.put("pausedOk", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testSuccess/pausedOkNeg.gif"))));
+			scenarioAsTestpausedMarkedNegMap.put("pausedErr", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testError/pausedErrorNeg.gif"))));
+			scenarioAsTestpausedMarkedNegMap.put("pausedWarn", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testWarning/pausedWarningNeg.gif"))));
+			scenarioAsTestpausedMarkedNegMap.put("pausedFail", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testFailure/pausedFailNeg.gif"))));
+			
+			scenarioAsTestpausedMarkedIsseuMap = new HashMap<>();
+			scenarioAsTestpausedMarkedIsseuMap.put("pausedPlainRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/plainTest/pausedIssueRun.gif"))));
+			scenarioAsTestpausedMarkedIsseuMap.put("pausedPlain", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/plainTest/pausedIssue.gif"))));
+			scenarioAsTestpausedMarkedIsseuMap.put("pausedOk", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testSuccess/pausedOkIssue.gif"))));
+			scenarioAsTestpausedMarkedIsseuMap.put("pausedErr", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testError/pausedErrorIssue.gif"))));
+			scenarioAsTestpausedMarkedIsseuMap.put("pausedWarn", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testWarning/pausedWarningIssue.gif"))));
+			scenarioAsTestpausedMarkedIsseuMap.put("pausedFail", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testFailure/pausedFailIssue.gif"))));
+			
+			scenarioAsTestpausedMarkedNegIssueMap = new HashMap<>();
+			scenarioAsTestpausedMarkedNegIssueMap.put("pausedPlainRun", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/plainTest/pausedRunIssueNeg.gif"))));
+			scenarioAsTestpausedMarkedNegIssueMap.put("pausedPlain", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/plainTest/pausedIssueNeg.gif"))));
+			scenarioAsTestpausedMarkedNegIssueMap.put("pausedOk", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testSuccess/pausedOkIssueNeg.gif"))));
+			scenarioAsTestpausedMarkedNegIssueMap.put("pausedErr", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testError/pausedErrorIssueNeg.gif"))));
+			scenarioAsTestpausedMarkedNegIssueMap.put("pausedWarn", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testWarning/pausedWarningIssueNeg.gif"))));
+			scenarioAsTestpausedMarkedNegIssueMap.put("pausedFail", new ImageIcon(IOUtils.toByteArray(cl.getResourceAsStream("scenarioAsTestIcons/testFailure/pausedFailIssueNeg.gif"))));
+		
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -106,7 +114,6 @@ public class IconsMapContainer {
 	public HashMap<String,ImageIcon> getPausedNormalMap(){
 		return pausedNormalMap;
 	}
-	
 	public HashMap<String,ImageIcon> getPausedMarkedNegMap(){
 		return pausedMarkedNegMap;
 	}
@@ -116,6 +123,20 @@ public class IconsMapContainer {
 	public HashMap<String,ImageIcon> getPausedMarkedNegIssueMap(){
 		return pausedMarkedNegIssueMap;
 	}
+	public HashMap<String,ImageIcon> getScenarioAsTestpausedMarkedNegIssueMap() {
+		return scenarioAsTestpausedMarkedNegIssueMap;
+	}
+	public HashMap<String,ImageIcon> getScenarioAsTestpausedMarkedIsseuMap() {
+		return scenarioAsTestpausedMarkedIsseuMap;
+	}
+	public HashMap<String,ImageIcon> getScenarioAsTestpausedMarkedNegMap() {
+		return scenarioAsTestpausedMarkedNegMap;
+	}
+	public HashMap<String,ImageIcon> getScenarioAsTestPausedNormalMap() {
+		return scenarioAsTestPausedNormalMap;
+	}
+
+	
 	
 
 	
--- src/main/java/org/jsystemtest/ScenarioRendererWithPause.java
+++ src/main/java/org/jsystemtest/ScenarioRendererWithPause.java
@@ -17,12 +17,13 @@ import jsystem.treeui.teststable.ScenarioTreeNode;
 public class ScenarioRendererWithPause extends ScenarioRenderer {
 
 	private final List<JTest> selectedTests;
-	private IconsMapContainer iconsMapContainer;
+	private static IconsMapContainer iconsMapContainer;
 
 	public ScenarioRendererWithPause(List<JTest> selectedTests) {
 
 		this.selectedTests = selectedTests;
-		iconsMapContainer = new IconsMapContainer();
+		if(iconsMapContainer == null)
+			iconsMapContainer = new IconsMapContainer();
 	}
 
 	@Override
@@ -44,13 +45,13 @@ public class ScenarioRendererWithPause extends ScenarioRenderer {
 			final boolean scenarioMarkedAsNegativeTest = scenarioAsTest.isMarkedAsNegativeTest();
 			
 			if (!scenarioMarkedAsKnownIssue && !scenarioMarkedAsNegativeTest) {
-				scenarioIconStatusSetter(scenarioAsTest, panel, iconsMapContainer.getPausedNormalMap());
+				scenarioIconStatusSetter(scenarioAsTest, panel, iconsMapContainer.getScenarioAsTestPausedNormalMap());
 			} else if (!scenarioMarkedAsKnownIssue && scenarioMarkedAsNegativeTest) {
-				scenarioIconStatusSetter(scenarioAsTest, panel, iconsMapContainer.getPausedMarkedNegMap());
+				scenarioIconStatusSetter(scenarioAsTest, panel, iconsMapContainer.getScenarioAsTestpausedMarkedNegMap());
 			} else if (scenarioMarkedAsKnownIssue && !scenarioMarkedAsNegativeTest) {
-				scenarioIconStatusSetter(scenarioAsTest, panel, iconsMapContainer.getPausedMarkedIsseuMap());
+				scenarioIconStatusSetter(scenarioAsTest, panel, iconsMapContainer.getScenarioAsTestpausedMarkedIsseuMap());
 			} else if (scenarioMarkedAsKnownIssue && scenarioMarkedAsNegativeTest) {
-				scenarioIconStatusSetter(scenarioAsTest, panel, iconsMapContainer.getPausedMarkedNegIssueMap());
+				scenarioIconStatusSetter(scenarioAsTest, panel, iconsMapContainer.getScenarioAsTestpausedMarkedNegIssueMap());
 			}
 		} else if (node.getTest() instanceof RunnerTest) {
 
@@ -72,82 +73,25 @@ public class ScenarioRendererWithPause extends ScenarioRenderer {
 
 	private void scenarioIconStatusSetter(Scenario scenarioAsTest, TreeJPanel panel, HashMap<String, ImageIcon> pausedIconMap) {
 		if (scenarioAsTest.isRunning()) {
-			System.out.println("********** Running");
+			panel.label.setIcon(pausedIconMap.get("pausedPlainRun"));
 		} else {
-			System.out.println("********** Not Running");
+			panel.label.setIcon(pausedIconMap.get("pausedPlain"));
 		}
 		if (scenarioAsTest.isFail()) {
-			System.out.println("********** Fail");
+			panel.label.setIcon(pausedIconMap.get("pausedFail"));
 		}
 		if (scenarioAsTest.isWarning()) {
-			System.out.println("********** Warning");
+			panel.label.setIcon(pausedIconMap.get("pausedWarn"));
 		}
 		if (scenarioAsTest.isError()) {
-			System.out.println("********** Error");
+			panel.label.setIcon(pausedIconMap.get("pausedErr"));
 		}
 		if (scenarioAsTest.isSuccess()) {
-			System.out.println("********** Success");
+			panel.label.setIcon(pausedIconMap.get("pausedOk"));
 		}
 		
 	}
 
-
-	// private void scenarioIconStatusSetter(Scenario test, TreeJPanel panel,
-	// HashMap<String, ImageIcon> pausedIconMap) {
-	//
-	// ScenarioAsTest scenarioAsTest = new ScenarioAsTest();
-	// scenarioAsTest.setCurrentRunnerTest(test);
-	// RunnerTest test2 = scenarioAsTest.getCurrentRunnerTest();
-	//
-	// if (test2.isRunning()) {
-	//
-	// switch (test2.getStatus()) {
-	//
-	// case RunnerTest.STAT_FAIL: {
-	// panel.label.setIcon(pausedIconMap.get("pausedFailRun"));
-	// }
-	// break;
-	// case RunnerTest.STAT_SUCCESS: {
-	// panel.label.setIcon(pausedIconMap.get("pausedOkRun"));
-	// }
-	// break;
-	// case RunnerTest.STAT_ERROR: {
-	// panel.label.setIcon(pausedIconMap.get("pausedErrRun"));
-	// }
-	// break;
-	// case RunnerTest.STAT_WARNING: {
-	// panel.label.setIcon(pausedIconMap.get("pausedWarnRun"));
-	// }
-	// break;
-	// default:
-	// panel.label.setIcon(pausedIconMap.get("pausedPlainRun"));
-	// }
-	// } else if (!test2.isRunning()) {
-	// switch (test2.getStatus()) {
-	//
-	// case RunnerTest.STAT_FAIL: {
-	// panel.label.setIcon(pausedIconMap.get("pausedFail"));
-	// }
-	// break;
-	// case RunnerTest.STAT_SUCCESS: {
-	// panel.label.setIcon(pausedIconMap.get("pausedOk"));
-	// }
-	// break;
-	// case RunnerTest.STAT_ERROR: {
-	// panel.label.setIcon(pausedIconMap.get("pausedErr"));
-	// }
-	// break;
-	// case RunnerTest.STAT_WARNING: {
-	// panel.label.setIcon(pausedIconMap.get("pausedWarn"));
-	// }
-	// break;
-	// default:
-	// panel.label.setIcon(pausedIconMap.get("pausedPlain"));
-	// }
-	// }
-	// }
-
-	//
 
 	private void testIconStatusSetter(RunnerTest test, TreeJPanel panel, HashMap<String, ImageIcon> pausedIconMap) {
 		if (test.isRunning()) {
